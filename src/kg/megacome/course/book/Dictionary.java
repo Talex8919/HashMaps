35493801412bd88e3708ea2cc8dc9b7da9104f55
Alexander <tianalex999@gmail.com>
2021-03-12 23:12:57 +0600

    First

---
+++ src/kg/megacome/course/book/Dictionary.java
@@ -0,0 +1,19 @@
+package kg.megacome.course.book;
+
+public class Dictionary {
+    private String words ;
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+
+    public Dictionary(String words) {
+        this.words = words;
+    }
+    public Dictionary() {
+    }
+}
